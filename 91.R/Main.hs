59b299aa29d47594e4926ef68032959d74f65d39
Phil K. Owen <hiroki@yairilab.net>
2018-08-21 00:05:32 +0900

    R interop

---
+++ 91.R/Main.hs
@@ -0,0 +1,23 @@
+import Foreign.R.Type as R
+import Foreign.R as R(cast)
+import Language.R.Instance(runRegion)
+import Language.R.QQ
+import Language.R.HExp as HExp
+import Data.Vector.SEXP as R(head, toList)
+
+fourtyTwo :: IO Double
+fourtyTwo = runRegion $ do
+    ans <- [r| 42 |]
+    let HExp.Real xHexp = hexp $ (R.cast R.SReal) ans
+    return $ R.head xHexp
+
+numSeq :: IO [Double]
+numSeq = runRegion $ do
+    ans <- [r| c(1, 2, 3, 6, 11, 23, 47) |]
+    let HExp.Real xHexp = hexp $ (R.cast R.SReal) ans
+    return $ R.toList xHexp
+
+main :: IO ()
+main = do
+    fourtyTwo >>= print
+    numSeq >>= print
