ac34d00150c3429428b4f1bccaf0da6ddd80073a
Phil K. Owen <hiroki@yairilab.net>
2018-08-20 21:04:34 +0900

    turtle os command

---
+++ 42.OSコマンド/Main.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Turtle
+
+main :: IO ()
+main = do
+  proc "say"    ["hello, how are you?"] empty
+  proc "wget"   ["http://www.nch.com.au/acm/8kmp38.wav"] empty
+  proc "afplay" ["8kmp38.wav"] empty
+  return ()
